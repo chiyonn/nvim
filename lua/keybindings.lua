53f42315c3583ee451939fa46880a4cea110cf07
chiyonn <kud0h.chi4@gmail.com>
2024-07-24 11:04:34 +0900

    update

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -2,11 +2,21 @@
 vim.g.mapleader = ' '
 
 -- Window navigation keybindings
-vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<Esc><Esc>', ':nohlsearch<CR>', { noremap = true, silent = true })
+local keymap = vim.api.nvim_set_keymap
+local options = { noremap = true, silent = true }
+
+keymap('n', 'j', 'gj', options)
+keymap('n', 'k', 'gk', options)
+keymap('n', '<C-h>', '<C-w>h', options)
+keymap('n', '<C-j>', '<C-w>j', options)
+keymap('n', '<C-k>', '<C-w>k', options)
+keymap('n', '<C-l>', '<C-w>l', options)
+keymap('n', '<Esc><Esc>', ':nohlsearch<CR>', options)
+
+-- 行選択モードでインデント後にビジュアルモードを維持する設定
+vim.api.nvim_set_keymap('v', '>', '>gv', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<', '<gv', { noremap = true, silent = true })
 
 -- NvimTree toggle keybinding
-vim.api.nvim_set_keymap('n', '<leader>e', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+keymap('n', '<leader>e', ':NvimTreeToggle<CR>', options)
+
--- lua/settings.lua
+++ lua/settings.lua
@@ -1,15 +1,23 @@
+local vim = vim
+
 -- Line numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
+
+-- Appearance
+vim.opt.colorcolumn = "81"
+vim.cmd [[highlight ColorColumn ctermbg=grey guibg=#808080]]
+
+vim.opt.scrolloff = 7
 
 -- Clipboard
 vim.opt.clipboard = 'unnamedplus'
 
 -- Tab and indentation settings
 vim.opt.expandtab = true     -- Use spaces instead of tabs
-vim.opt.shiftwidth = 4       -- Number of spaces to use for each step of (auto)indent
-vim.opt.tabstop = 4          -- Number of spaces that a <Tab> in the file counts for
-vim.opt.softtabstop = 4      -- Number of spaces that a <Tab> counts for while performing editing operations
+vim.opt.shiftwidth = 2       -- Number of spaces to use for each step of (auto)indent
+vim.opt.tabstop = 2          -- Number of spaces that a <Tab> in the file counts for
+vim.opt.softtabstop = 2      -- Number of spaces that a <Tab> counts for while performing editing operations
 vim.opt.smartindent = true   -- Makes indenting smart
 
 -- Sign column (always show)
@@ -18,36 +26,24 @@ vim.opt.signcolumn = 'yes'
 vim.api.nvim_create_augroup("FileTypeIndent", {})
 vim.api.nvim_create_autocmd("FileType", {
   group = "FileTypeIndent",
-  pattern = "python",
+  pattern = "python,go",
   command = "setlocal tabstop=4 shiftwidth=4 expandtab",
 })
 vim.api.nvim_create_autocmd("FileType", {
   group = "FileTypeIndent",
-  pattern = "javascript",
-  command = "setlocal tabstop=2 shiftwidth=2 expandtab",
-})
-vim.api.nvim_create_autocmd("FileType", {
-  group = "FileTypeIndent",
-  pattern = "html",
-  command = "setlocal tabstop=2 shiftwidth=2 expandtab",
-})
-vim.api.nvim_create_autocmd("FileType", {
-  group = "FileTypeIndent",
-  pattern = "css",
-  command = "setlocal tabstop=2 shiftwidth=2 expandtab",
-})
-vim.api.nvim_create_autocmd("FileType", {
-  group = "FileTypeIndent",
-  pattern = "typescript",
-  command = "setlocal tabstop=2 shiftwidth=2 expandtab",
-})
-vim.api.nvim_create_autocmd("FileType", {
-  group = "FileTypeIndent",
-  pattern = "tsx",
-  command = "setlocal tabstop=2 shiftwidth=2 expandtab",
-})
-vim.api.nvim_create_autocmd("FileType", {
-  group = "FileTypeIndent",
   pattern = "go",
-  command = "setlocal tabstop=4 shiftwidth=4 noexpandtab",
+  command = "setlocal noexpandtab",
 })
+
+-- コマンド履歴の保存
+if vim.fn.has("persistent_undo") == 1 then
+    local target_path = vim.fn.expand("~/.config/nvim/undodir")
+    if vim.fn.isdirectory(target_path) == 0 then
+        vim.fn.mkdir(target_path, "p", "0700")
+    end
+    vim.opt.undodir = target_path
+    vim.opt.undofile = true
+end
+
+-- 検索履歴とコマンド履歴の保存
+vim.opt.viminfo = "'100,<1000,s10,h"
