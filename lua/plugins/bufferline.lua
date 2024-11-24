1b0ac148b3f7b802b757a6f815287fa16dea340e
chiyonn <kud0h.chi4@gmail.com>
2024-11-24 13:32:31 +0900

    バッファーラインの追加

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,23 @@
+return {
+  "akinsho/bufferline.nvim",
+  dependencies = "nvim-tree/nvim-web-devicons",
+  config = function()
+    require("bufferline").setup({
+      options = {
+        diagnostics = "nvim_lsp", -- LSP診断情報を表示
+        offsets = {
+          {
+            filetype = "NvimTree",
+            text = "File Explorer",
+            text_align = "center",
+          },
+        },
+        separator_style = "slant", -- タブ間のスタイル
+      },
+    })
+
+    -- バッファの移動用キーマッピングを設定
+    vim.keymap.set("n", "J", ":BufferLineCyclePrev<CR>", { noremap = true, silent = true })
+    vim.keymap.set("n", "K", ":BufferLineCycleNext<CR>", { noremap = true, silent = true })
+  end,
+}
