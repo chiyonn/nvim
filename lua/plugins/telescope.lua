af22cd446be6052ee01d8279c8dedd5471e69d2e
chiyonn <kud0h.chi4@gmail.com>
2024-11-25 10:52:43 +0900

    キーバインドによるfind_files関数をインポートしてから使用

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,8 +1,10 @@
+-- Minimal Telescope setup with <leader>f keybinding
 return {
   'nvim-telescope/telescope.nvim',
   dependencies = { 'nvim-lua/plenary.nvim' }, -- Required dependency
   config = function()
     local telescope = require('telescope')
+    local builtin = require('telescope.builtin') -- Import 'telescope.builtin'
 
     telescope.setup({
       defaults = {
@@ -13,6 +15,6 @@ return {
     })
 
     -- Keymap for <leader>f to open Telescope find_files
-    vim.keymap.set('n', '<leader>f', telescope.builtin.find_files, { desc = "Telescope Find Files" })
+    vim.keymap.set('n', '<leader>f', builtin.find_files, { desc = "Telescope Find Files" })
   end
 }
