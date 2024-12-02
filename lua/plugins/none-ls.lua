843f894e1e6d918ab0a8109ad35688a6535d1bfd
chiyonn <kud0h.chi4@gmail.com>
2024-12-02 13:36:40 +0900

    React/TSのフォーマットを追加

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -31,13 +31,19 @@ return {
           extra_args = { "--indent", "4" }, -- インデント幅の指定
           filetypes = { "htmldjango" }, -- Jinja2ファイルに限定
         }),
+
+        -- TypeScript/JavaScript (React) の整形ツール
+        none_ls.builtins.formatting.prettier.with({
+          filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" }, -- React/TypeScript 用
+          extra_args = { "--print-width", "80", "--single-quote", "--jsx-single-quote" }, -- プリティア設定
+        }),
       },
     })
 
     -- 保存時に自動でフォーマットを実行
     vim.api.nvim_create_autocmd("BufWritePre", {
       group = vim.api.nvim_create_augroup("AutoFormat", { clear = true }),
-      pattern = { "*.html", "*.jinja", "*.jinja2", "*.css", "*.scss", "*.less" },
+      pattern = { "*.html", "*.jinja", "*.jinja2", "*.css", "*.scss", "*.less", "*.ts", "*.tsx", "*.js", "*.jsx" },
       callback = function()
         vim.lsp.buf.format()
       end,
