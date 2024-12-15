163ee2c01fdc376788a5b7bbd7353d15d6502683
chiyonn <kud0h.chi4@gmail.com>
2024-12-15 13:17:02 +0900

    normalモードのみキーマッピング作動

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,12 +1,15 @@
-return {
-  "akinsho/toggleterm.nvim",
+return { "akinsho/toggleterm.nvim",
   version = "*",
   config = function()
     require("toggleterm").setup({
       size = 20, -- デフォルトの高さ/幅
-      open_mapping = [[<leader>t]], -- トグル用のキーマッピング
       direction = "horizontal", -- horizontal, vertical, float から選択
       shell = vim.o.shell, -- 使用するシェル
     })
+
+    -- Normalモード専用のキーマッピング
+    vim.keymap.set("n", "<leader>t", function()
+      require("toggleterm").toggle()
+    end, { noremap = true, silent = true })
   end,
 }
