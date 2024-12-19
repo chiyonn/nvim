c580b680a9b94e0b68dcc8a947bcfd6532613e02
chiyonn <kud0h.chi4@gmail.com>
2024-12-20 07:36:03 +0900

    [add]: gofmtとgoimportsを保存時に適用

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -3,14 +3,33 @@ return {
   config = function()
     local lspconfig = require("lspconfig")
 
+    -- 保存時にフォーマットを実行する共通の on_attach 関数
+    local function on_attach(client, bufnr)
+      -- フォーマット機能が有効な場合のみ設定
+      if client.server_capabilities.documentFormattingProvider then
+        vim.api.nvim_create_autocmd("BufWritePre", {
+          group = vim.api.nvim_create_augroup("FormatOnSave", { clear = true }),
+          buffer = bufnr,
+          callback = function()
+            vim.lsp.buf.format({ async = false })
+          end,
+        })
+      end
+    end
+
     -- Python 用 (pyright)
-    lspconfig.pyright.setup({})
+    lspconfig.pyright.setup({
+      on_attach = on_attach,
+    })
 
     -- TypeScript/JavaScript 用 (tsserver)
-    lspconfig.ts_ls.setup({})
+    lspconfig.tsserver.setup({
+      on_attach = on_attach,
+    })
 
     -- Lua 用 (lua_ls)
     lspconfig.lua_ls.setup({
+      on_attach = on_attach,
       settings = {
         Lua = {
           runtime = {
@@ -26,6 +45,20 @@ return {
           telemetry = {
             enable = false, -- 適宜、テレメトリを無効化
           },
+        },
+      },
+    })
+
+    -- Go 用 (gopls)
+    lspconfig.gopls.setup({
+      on_attach = on_attach,
+      settings = {
+        gopls = {
+          gofumpt = true, -- gofumpt によるフォーマットを有効化
+          analyses = {
+            unusedparams = true, -- 未使用パラメータの警告を有効化
+          },
+          staticcheck = true, -- staticcheck を有効化
         },
       },
     })
