d61ee80bf8c8dd223f380de83666b2d15ac578c5
chiyonn <kud0h.chi4@gmail.com>
2024-12-27 10:47:34 +0900

    [add]: TreeSitterがconfigに参加しました。

---
+++ lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,22 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+        require("nvim-treesitter.configs").setup({
+            -- 有効化するモジュール
+            highlight = {
+                enable = true, -- Tree-sitter によるハイライトを有効化
+                additional_vim_regex_highlighting = false,
+            },
+            indent = {
+                enable = true, -- Tree-sitter ベースのインデントを有効化
+            },
+            -- インストールする言語パーサー
+            ensure_installed = {
+                "lua", "javascript", "typescript", "python", "html", "css", "bash", "json",
+            },
+            -- 未インストールのパーサーを自動インストール
+            auto_install = true,
+        })
+    end,
+}
