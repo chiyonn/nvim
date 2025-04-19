5b71a0ba2489aa54ecef3e0533636f71b9672530
chiyonn <kud0h.chi4@gmail.com>
2025-04-19 13:46:35 +0900

    udpate: add python file to pattern

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -43,7 +43,7 @@ return {
     -- 保存時に自動でフォーマットを実行
     vim.api.nvim_create_autocmd("BufWritePre", {
       group = vim.api.nvim_create_augroup("AutoFormat", { clear = true }),
-      pattern = { "*.html", "*.jinja", "*.jinja2", "*.css", "*.scss", "*.less", "*.ts", "*.tsx", "*.js", "*.jsx" },
+      pattern = { "*.html", "*.jinja", "*.jinja2", "*.css", "*.scss", "*.less", "*.ts", "*.tsx", "*.js", "*.jsx", "*.py" },
       callback = function()
         vim.lsp.buf.format()
       end,
