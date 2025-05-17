315bd72553830cff0e5ec34449ea1345b1a55eea
chiyonn <kud0h.chi4@gmail.com>
2025-05-17 20:51:48 +0900

    update: use mason to manage lsps

---
+++ lua/plugins/mason-lspconfig.lua
@@ -0,0 +1,14 @@
+return {
+  "williamboman/mason-lspconfig.nvim",
+  dependencies = { "williamboman/mason.nvim" },
+  opts = {
+    ensure_installed = {
+      "lua_ls",
+      "pyright",
+      "ts_ls",
+      "gopls",
+    },
+    automatic_installation = true,
+  },
+}
+
--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,70 +1,70 @@
 return {
-  "neovim/nvim-lspconfig",
-  config = function()
-    local lspconfig = require("lspconfig")
+    "neovim/nvim-lspconfig",
+    config = function()
+        local lspconfig = require("lspconfig")
 
-    local function on_attach(client, bufnr)
-      if client.server_capabilities.documentFormattingProvider then
-        vim.api.nvim_create_autocmd("BufWritePre", {
-          group = vim.api.nvim_create_augroup("FormatOnSave", { clear = true }),
-          buffer = bufnr,
-          callback = function()
-            vim.lsp.buf.format({ async = false })
-          end,
+        local function on_attach(client, bufnr)
+            if client.server_capabilities.documentFormattingProvider then
+                vim.api.nvim_create_autocmd("BufWritePre", {
+                    group = vim.api.nvim_create_augroup("FormatOnSave", { clear = true }),
+                    buffer = bufnr,
+                    callback = function()
+                        vim.lsp.buf.format({ async = false })
+                    end,
+                })
+            end
+
+            local opts = { buffer = bufnr, silent = true }
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+            vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+            vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+        end
+
+        -- Python
+        lspconfig.pyright.setup({
+            on_attach = on_attach,
         })
-      end
 
-      local opts = { buffer = bufnr, silent = true }
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-      vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-    end
+        -- TypeScript/JavaScript
+        lspconfig.ts_ls.setup({
+            on_attach = on_attach,
+        })
 
-    -- Python
-    lspconfig.pyright.setup({
-      on_attach = on_attach,
-    })
+        -- Lua
+        lspconfig.lua_ls.setup({
+            on_attach = on_attach,
+            settings = {
+                Lua = {
+                    runtime = {
+                        version = "LuaJIT",
+                    },
+                    diagnostics = {
+                        globals = { "vim" },
+                    },
+                    workspace = {
+                        library = vim.api.nvim_get_runtime_file("", true),
+                        checkThirdParty = false,
+                    },
+                    telemetry = {
+                        enable = false,
+                    },
+                },
+            },
+        })
 
-    -- TypeScript/JavaScript
-    lspconfig.ts_ls.setup({
-      on_attach = on_attach,
-    })
-
-    -- Lua
-    lspconfig.lua_ls.setup({
-      on_attach = on_attach,
-      settings = {
-        Lua = {
-          runtime = {
-            version = "LuaJIT",
-          },
-          diagnostics = {
-            globals = { "vim" },
-          },
-          workspace = {
-            library = vim.api.nvim_get_runtime_file("", true),
-            checkThirdParty = false,
-          },
-          telemetry = {
-            enable = false,
-          },
-        },
-      },
-    })
-
-    -- Go
-    lspconfig.gopls.setup({
-      on_attach = on_attach,
-      settings = {
-        gopls = {
-          gofumpt = true,
-          analyses = {
-            unusedparams = true,
-          },
-          staticcheck = true,
-        },
-      },
-    })
-  end,
+        -- Go
+        lspconfig.gopls.setup({
+            on_attach = on_attach,
+            settings = {
+                gopls = {
+                    gofumpt = true,
+                    analyses = {
+                        unusedparams = true,
+                    },
+                    staticcheck = true,
+                },
+            },
+        })
+    end,
 }
