c82be430865e5a641d5025faab30efefdae7bf2b
chiyonn <kud0h.chi4@gmail.com>
2025-05-18 16:42:32 +0900

    update: format and changed a model

--- lua/plugins/ollama.lua
+++ lua/plugins/ollama.lua
@@ -1,40 +1,40 @@
 return {
-  "nomnivore/ollama.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
-
-  -- All the user commands added by the plugin
-  cmd = { "Ollama", "OllamaModel", "OllamaServe", "OllamaServeStop" },
-
-  keys = {
-    -- Sample keybind for prompt menu. Note that the <c-u> is important for selections to work properly.
-    {
-      "<leader>oo",
-      ":<c-u>lua require('ollama').prompt()<cr>",
-      desc = "ollama prompt",
-      mode = { "n", "v" },
+    "nomnivore/ollama.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
     },
 
-    {
-      "<leader>oM",
-      ":<c-u>lua require('ollama').prompt('Modify_Code')<cr>",
-      desc = "ollama Modify Code",
-      mode = { "n", "v" },
+    -- All the user commands added by the plugin
+    cmd = { "Ollama", "OllamaModel", "OllamaServe", "OllamaServeStop" },
+
+    keys = {
+        -- Sample keybind for prompt menu. Note that the <c-u> is important for selections to work properly.
+        {
+            "<leader>oo",
+            ":<c-u>lua require('ollama').prompt()<cr>",
+            desc = "ollama prompt",
+            mode = { "n", "v" },
+        },
+
+        {
+            "<leader>oM",
+            ":<c-u>lua require('ollama').prompt('Modify_Code')<cr>",
+            desc = "ollama Modify Code",
+            mode = { "n", "v" },
+        },
+
+        -- Sample keybind for direct prompting. Note that the <c-u> is important for selections to work properly.
+        {
+            "<leader>oG",
+            ":<c-u>lua require('ollama').prompt('Generate_Code')<cr>",
+            desc = "ollama Generate Code",
+            mode = { "n", "v" },
+        },
     },
 
-    -- Sample keybind for direct prompting. Note that the <c-u> is important for selections to work properly.
-    {
-      "<leader>oG",
-      ":<c-u>lua require('ollama').prompt('Generate_Code')<cr>",
-      desc = "ollama Generate Code",
-      mode = { "n", "v" },
-    },
-  },
-
---@type Ollama.Config
-  opts = {
-    model = "mistral",
-    url = "http://192.168.40.100:11434",
-  }
+    --@type Ollama.Config
+    opts = {
+        model = "qwen3:32b",
+        url = "http://192.168.40.100:11434",
+    }
 }
