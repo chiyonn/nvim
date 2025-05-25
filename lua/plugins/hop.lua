1b3bb3b5499b59bf4581ee14dd69397b60eeb8ba
chiyonn <kud0h.chi4@gmail.com>
2025-05-25 10:10:51 +0900

    update: changed key map to call hop

--- lua/plugins/hop.lua
+++ lua/plugins/hop.lua
@@ -11,13 +11,8 @@ return {
 
     local map = vim.keymap.set
 
-    map("n", "gw", function()
+    map("n", "<leader>f", function()
       hop.hint_words()
     end, { desc = "Hop to Word" })
-
-    -- リーダーキー派の例
-    -- map("n", "<leader>w", function()
-    --   hop.hint_words()
-    -- end, { desc = "Hop to Word" })
   end,
 }
