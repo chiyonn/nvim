7ff90d260cd55b9d7da1e99b1ac7f9061258168a
chiyonn <kud0h.chi4@gmail.com>
2025-06-15 12:18:57 +0900

    update: add a snippet to create construct pattern

--- snippets/go.lua
+++ snippets/go.lua
@@ -2,22 +2,43 @@ local ls = require("luasnip")
 local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
+local rep = require("luasnip.extras").rep
+local fmt = require("luasnip.extras.fmt").fmt
 
 ls.add_snippets("go", {
-    s(
-        "errnew",
-        {
-            t("return nil, errors.New(\""),
-            i(1),
-            t("\")"),
-        }
-    ),
-    s(
-        "errf",
-        {
-            t("return nil, fmt.Errorf(\""),
-            i(1),
-            t("\")"),
-        }
-    ),
+  s(
+    "errnew",
+    {
+      t("return nil, errors.New(\""),
+      i(1),
+      t("\")"),
+    }
+  ),
+  s(
+    "errf",
+    {
+      t("return nil, fmt.Errorf(\""),
+      i(1),
+      t("\")"),
+    }
+  ),
+})
+
+
+ls.add_snippets("go", {
+  s("new", fmt([[
+        type {} struct {{
+            {}
+        }}
+
+        func New{}() *{} {{
+            return &{}{{}}
+        }}
+    ]], {
+    i(1, "MyType"),    -- オブジェクト名にカーソルが当たる
+    i(2, "// fields"), -- フィールド
+    rep(1),            -- 関数名の NewXxx にも同じ名前を使用
+    rep(1),            -- 戻り値の型も同じ
+    rep(1),            -- return &Xxx{} の中も同じ
+  }))
 })
