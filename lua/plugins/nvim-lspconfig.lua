22d57f5f27d2301d0865599643c34cb92e76c302
chiyonn <kud0h.chi4@gmail.com>
2025-06-21 11:08:19 +0900

    refactor:

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,105 +1,85 @@
 return {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-        "hrsh7th/cmp-nvim-lsp",
-    },
-    config = function()
-        vim.diagnostic.config({
-            virtual_text = true,
-            signs = true,
-            underline = true,
-            update_in_insert = true,
-            severity_sort = true,
+  "neovim/nvim-lspconfig",
+  dependencies = { "hrsh7th/cmp-nvim-lsp" },
+
+  config = function()
+    local lspconfig = require("lspconfig")
+    local cmp_nvim_lsp = require("cmp_nvim_lsp")
+
+    -- Diagnostic UI settings
+    vim.diagnostic.config({
+      virtual_text = true,
+      signs = true,
+      underline = true,
+      update_in_insert = true,
+      severity_sort = true,
+    })
+
+    local capabilities = cmp_nvim_lsp.default_capabilities()
+
+    -- Format on save if server supports it
+    local function setup_format_on_save(client, bufnr)
+      if client.name == "gopls" and client.server_capabilities.documentFormattingProvider then
+        vim.api.nvim_create_autocmd("BufWritePre", {
+          group = vim.api.nvim_create_augroup("GoFormatOnSave", { clear = true }),
+          buffer = bufnr,
+          callback = function()
+            vim.lsp.buf.format({ async = false })
+          end,
         })
+      end
+    end
 
-        local lspconfig = require("lspconfig")
-        local cmp_nvim_lsp = require("cmp_nvim_lsp")
+    -- LSP attach handler
+    local function on_attach(client, bufnr)
+      setup_format_on_save(client, bufnr)
 
-        local capabilities = cmp_nvim_lsp.default_capabilities()
+      local opts = { buffer = bufnr, silent = true }
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+      vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+    end
 
-        local function on_attach(client, bufnr)
-            if client.server_capabilities.documentFormattingProvider then
-                vim.api.nvim_create_autocmd("BufWritePre", {
-                    group = vim.api.nvim_create_augroup("FormatOnSave", { clear = true }),
-                    buffer = bufnr,
-                    callback = function()
-                        vim.lsp.buf.format({ async = false })
-                    end,
-                })
-            end
-
-            local opts = { buffer = bufnr, silent = true }
-            vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-            vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-            vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-        end
-
-        -- Python
-        lspconfig.pyright.setup({
-            on_attach = on_attach,
-            capabilities = capabilities,
-        })
-
-        -- TypeScript/JavaScript
-        lspconfig.ts_ls.setup({
-            on_attach = on_attach,
-            capabilities = capabilities,
-        })
-
-        -- Lua
-        lspconfig.lua_ls.setup({
-            on_attach = on_attach,
-            capabilities = capabilities,
-            settings = {
-                Lua = {
-                    runtime = {
-                        version = "LuaJIT",
-                    },
-                    diagnostics = {
-                        globals = { "vim" },
-                    },
-                    workspace = {
-                        library = vim.api.nvim_get_runtime_file("", true),
-                        checkThirdParty = false,
-                    },
-                    telemetry = {
-                        enable = false,
-                    },
-                },
+    -- Language servers
+    local servers = {
+      pyright = {},
+      tsserver = {},
+      lua_ls = {
+        settings = {
+          Lua = {
+            runtime = { version = "LuaJIT" },
+            diagnostics = { globals = { "vim" } },
+            workspace = {
+              library = vim.api.nvim_get_runtime_file("", true),
+              checkThirdParty = false,
             },
-        })
+            telemetry = { enable = false },
+          },
+        },
+      },
+      gopls = {
+        settings = {
+          gopls = {
+            gofumpt = true,
+            analyses = { unusedparams = true },
+            staticcheck = true,
+          },
+        },
+      },
+      cssls = {
+        settings = {
+          css = { validate = true },
+          scss = { validate = true },
+          less = { validate = true },
+        },
+      },
+    }
 
-        -- Go
-        lspconfig.gopls.setup({
-            on_attach = on_attach,
-            capabilities = capabilities,
-            settings = {
-                gopls = {
-                    gofumpt = true,
-                    analyses = {
-                        unusedparams = true,
-                    },
-                    staticcheck = true,
-                },
-            },
-        })
-
-        -- CSS
-        lspconfig.cssls.setup({
-            on_attach = on_attach,
-            capabilities = capabilities,
-            settings = {
-                css = {
-                    validate = true,
-                },
-                scss = {
-                    validate = true,
-                },
-                less = {
-                    validate = true,
-                },
-            },
-        })
-    end,
+    for name, config in pairs(servers) do
+      config.on_attach = on_attach
+      config.capabilities = capabilities
+      lspconfig[name].setup(config)
+    end
+  end,
 }
