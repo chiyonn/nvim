db62fe922e4a5f0b8d021db14c42a6c49c480a72
chiyonn <kud0h.chi4@gmail.com>
2025-12-15 08:23:37 +0900

    update: follow new style API for markdown renderer

--- init.lua
+++ init.lua
@@ -1,6 +1,9 @@
 -- Add mise shims to PATH for Mason
 vim.env.PATH = vim.env.HOME .. "/.local/share/mise/shims:" .. vim.env.PATH
 
+-- Python provider
+vim.g.python3_host_prog = vim.env.HOME .. "/.local/share/nvim/python-env/bin/python"
+
 -- Leader key configuration
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -2,50 +2,33 @@ return {
     "MeanderingProgrammer/render-markdown.nvim",
     dependencies = {
         "nvim-treesitter/nvim-treesitter",
-        "lukas-reineke/indent-blankline.nvim",
+        "nvim-tree/nvim-web-devicons",
     },
-    ft = { "markdown", "codecompanion" }, -- ここで filetypes を追加
-    config = function()
-        require("render-markdown").setup({
-            -- filetypes を明示することで拡張 filetype をカバー
-            filetypes = { "markdown", "codecompanion" },
-            headings = {
-                "█", "▓", "▒", "░", "∙", "·",
+    ft = { "markdown", "codecompanion" },
+    opts = {
+        file_types = { "markdown", "codecompanion" },
+        heading = {
+            icons = { "█ ", "▓ ", "▒ ", "░ ", "∙ ", "· " },
+        },
+        bullet = {
+            icons = { "•", "◦", "▪", "▫" },
+        },
+        link = {
+            enabled = true,
+            wiki = {
+                icon = "󰌹 ",
+                highlight = "RenderMarkdownWikiLink",
             },
-            bullets = { "•", "◦", "▪", "▫" },
-            highlights = {
-                heading = {
-                    H1 = "markdownH1",
-                    H2 = "markdownH2",
-                    H3 = "markdownH3",
-                    H4 = "markdownH4",
-                    H5 = "markdownH5",
-                    H6 = "markdownH6",
-                },
-                code = "markdownCode",
-                blockquote = "Comment",
+        },
+        win_options = {
+            conceallevel = {
+                default = vim.o.conceallevel,
+                rendered = 3,
             },
-            conceal = true,
-            auto_enable = true,
-            -- Wikilink configuration
-            link = {
-                enabled = true,
-                wiki = {
-                    icon = '󰌹 ',  -- Icon displayed for wikilinks
-                    highlight = 'RenderMarkdownWikiLink',  -- Highlight group for wikilinks
-                },
+            concealcursor = {
+                default = vim.o.concealcursor,
+                rendered = "",
             },
-            -- Window options for concealment
-            win_options = {
-                conceallevel = {
-                    default = vim.o.conceallevel,
-                    rendered = 3,  -- Maximum concealment when rendered
-                },
-                concealcursor = {
-                    default = vim.o.concealcursor,
-                    rendered = '',  -- Show concealed text in all modes
-                },
-            },
-        })
-    end,
+        },
+    },
 }
